9c8092fdc11c175ebf06b27e00c4296ed6df4ea9
Jo <cocokcocok33@gmail.com>
2020-10-06 16:01:00 +0700

    Convert HTML to Svelte Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,4 @@
 <script>
-  import Home from "./pages/Home.Svelte";
+  import Home from "./pages/Home.svelte";
 </script>
 <Home/>
-
-<script>
-  import CharityList from "../components/CharityList.svelte";
-  import Header from "../components/Header.svelte";
-  import Welcome from "../components/Welcome.svelte";
-  import Promo from "../components/Promo.svelte";
-  import Footer from "../components/Footer.svelte";
-  import {charities} from "../data/charities.js";
-
-</script>
-
-  <Header/>
-  <Welcome/>
-  <CharityList {charities} />
-  <Promo/>
-  <Footer/>
