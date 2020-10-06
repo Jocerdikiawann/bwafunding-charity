72a205ed4b92091d7a2d8b14baf62a78ffc6d327
Jo <cocokcocok33@gmail.com>
2020-10-06 15:52:01 +0700

    Hapus home

--- src/App.svelte
+++ src/App.svelte
@@ -2,3 +2,19 @@
   import Home from "./pages/Home.Svelte";
 </script>
 <Home/>
+
+<script>
+  import CharityList from "../components/CharityList.svelte";
+  import Header from "../components/Header.svelte";
+  import Welcome from "../components/Welcome.svelte";
+  import Promo from "../components/Promo.svelte";
+  import Footer from "../components/Footer.svelte";
+  import {charities} from "../data/charities.js";
+
+</script>
+
+  <Header/>
+  <Welcome/>
+  <CharityList {charities} />
+  <Promo/>
+  <Footer/>
